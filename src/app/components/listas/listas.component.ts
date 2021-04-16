d6bdca07fe4790ccf58dace60560b4e7757f1db9
ibellon <isidro.bellon@gmail.com>
2021-04-16 11:34:44 +0200

    Componente listas y eliminar lista

---
+++ src/app/components/listas/listas.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Lista } from 'src/app/models/lista.model';
+import { DeseosService } from 'src/app/services/deseos.service';
+
+@Component({
+  selector: 'app-listas',
+  templateUrl: './listas.component.html',
+  styleUrls: ['./listas.component.scss'],
+})
+export class ListasComponent implements OnInit {
+
+  @Input() terminadas = true;
+
+  constructor(public deseosService: DeseosService,
+              private router: Router) { 
+
+  }
+
+  ngOnInit() {}
+
+  listaSeleccionada(lista: Lista) {
+
+    console.log(lista);
+
+    if( this.terminadas ) {
+      this.router.navigateByUrl('/tabs/tab2/agregar/'+lista.id);
+    }
+    else {
+      this.router.navigateByUrl('/tabs/tab1/agregar/'+lista.id);
+    }
+    
+  }
+
+  borrarLista(lista: Lista) {
+
+    this.deseosService.borrarLista(lista);
+
+  }
+}
